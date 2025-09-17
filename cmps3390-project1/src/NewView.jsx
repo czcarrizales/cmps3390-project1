4417af970eab503130b9060f416987d9fa900e3d
Chazz Carrizales <czcarrizales@yahoo.com>
2025-09-17 12:34:23 -0700

    make hello button, NewView, and npm i

--- cmps3390-project1/src/NewView.jsx
+++ cmps3390-project1/src/NewView.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function NewView() {
 
   return (
     <>
-      <button>Hello</button>
+      <button>New View</button>
     </>
   )
 }
 
-export default App
+export default NewView
