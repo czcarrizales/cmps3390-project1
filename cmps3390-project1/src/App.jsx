70abd0e64ce69c79a819e4bfc04dab3b1b5a1157
Chazz Carrizales <czcarrizales@yahoo.com>
2025-09-18 15:42:15 -0700

    add routing and new views to project

--- cmps3390-project1/src/App.jsx
+++ cmps3390-project1/src/App.jsx
@@ -1,14 +1,29 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import './App.css'
+import Home from './Home';
+import NewView from './NewView';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <button>Hello</button>
+    <h1>Testing Out</h1>
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/newview'>Contact</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/newview' element={<NewView />} />
+        </Routes>
+
+      </Router>
     </>
   )
 }
--- cmps3390-project1/src/Home.jsx
+++ cmps3390-project1/src/Home.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
-function NewView() {
+function Home() {
 
   return (
     <>
-      <h2>Contact</h2>
+      
+      <h2>Home</h2>
     </>
   )
 }
 
-export default NewView
+export default Home
